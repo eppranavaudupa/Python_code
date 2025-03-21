2337f2ecee61f169918ac036155f21d8d639f64b
pranava <eppranavudupa@gmail.com>
2025-03-21 22:11:57 +0530

    added New Patters

--- patternLogicQuestions.py
+++ patternLogicQuestions.py
@@ -1,4 +1,4 @@
-n=15
+n=10
 def triangleFilled(n):
     for i in range(n//2,n):
         for j in range(n):
@@ -9,4 +9,40 @@ def triangleFilled(n):
             elif(i-j>=0):
                 print("*",end="")
         print()
-triangleFilled(n)
+# triangleFilled(n)
+def q1(n):
+    for i in range(n):
+        for j in range(n):
+            if((i-j==0 and j<=n//2) or (i+j==n-1 and j<=n//2) or j==0):
+                print(" ",end="")
+            elif(i-j>0 and i+j<n-1 ):
+                print("*",end="")
+        print()
+# q1(n)
+def q2(n):
+    for i in range(n):
+        for j in range(n):
+            if(((j<=n//2 and i<n//2)or i>=n//2 and j>=n//2) ):
+                print("*",end="")
+            else:
+                print(" ",end="")
+        print()
+# q2(n)
+def q3(n):
+    for i in range(n):
+        for j in range(n):
+            if(not(i>=n//2 and j>=n//4 and i+j<=n-1)):
+                print("*",end="")
+            else:
+                print("$",end="")
+        print()
+# q3(n)
+def q4(n):
+    for i in range(n):
+        for j in range(n):
+            if(i+j>=n-1):
+                print("*",end="")
+            else:
+                print(" ",end="")
+        print()
+q4(n)
